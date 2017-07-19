2e945c1ba5d44d82b4d860436cdc2fb969ed0502
daicunya <daicunya@163.com>
2017-07-19 13:46:40 +0800

    修改SAT链接

--- commands/front/views/banner.php
+++ commands/front/views/banner.php
@@ -3,42 +3,9 @@
         <a href="/"><img class="logo" src="/cn/Hirsi/images/sy-logo.png" alt=""></a>
         <ul class="inb nav-list clearfix">
             <li><a class="on on-hover nav1-link" href="/">首页</a></li>
-            <li><a class="nav1-link" href="/study-aboard/assistance.html">留学服务</a>
-                <!--<div class="nav2-wrap">
-                    <div class="nav2-list">
-                        <div class="inb nav2-left-wrap">
-                            <div class="nav2-left">
-                                <div class="inb nav2-name">我要申请</div>
-                                <div class="inb nav2-mid1">
-                                    <a href="/Undergraduate.html">本科</a>
-                                    <a href="/Postgraduate.html">硕士</a>
-                                    <a href="/Doctor.html">博士</a>
-                                    <a href="/picture-details/127/index,111.html">MBA</a>
-                                    <br>
-                                    <a href="/USA.html">美国</a>
-                                    <a href="/UK.html">英国</a>
-                                    <a href="/HK.html">香港</a>
-                                    <a href="/AUS.html">澳洲</a>
-                                    <a href="/COUNTRY.html">其他</a>
-                                </div>
-                            </div>
-                            <div class="nav2-left">
-                                <div class="inb nav2-name">推荐服务</div>
-                                <div class="inb nav2-mid">
-                                    <a href="/picture-details/111/index,110.html">文书定制</a>
-                                    <a href="/picture-details/116/index,110.html">签证服务</a>
-                                    <a href="/picture-details/115/index,110.html">面试辅导</a>
-                                    <br>
-                                    <a href="/practices.html">实习</a>
-                                    <a href="/picture-details/118/index,110.html">Summer school</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>-->
-            </li>
+            <li><a class="nav1-link" href="/study-aboard/assistance.html">留学服务</a></li>
             <li><a class="nav1-link" href="/gmatsheet.html">GMAT课程</a></li>
-            <li><a class="nav1-link" href="http://sat.thinkuedu.com/" target="_blank">SAT课程</a></li>
+            <li><a class="nav1-link" href="http://sat.viplgw.cn/" target="_blank">SAT课程</a></li>
             <li><a class="nav1-link" href="/toefl/assistance.html">托福课程</a></li>
             <li><a class="nav1-link" href="/ieltssheet.html">雅思课程</a></li>
             <li><a class="nav1-link" href="/public-class.html">申友讲堂</a></li>
--- modules/cn/views/index/index.php
+++ modules/cn/views/index/index.php
@@ -157,68 +157,6 @@
             ?>
         </ul>
     </div>
-    <!--<div class="w10 info-wrap">
-        <div class="text-wrap">
-            <h1 class="info-tit">我的留学方案</h1>
-            <div class="user-info clearfix">
-                <div class="fl">
-                    <p class="info-tit">姓名：</p>
-                    <input class="info-int" id="username" type="text" placeholder="">
-                </div>
-                <div class="fr" style="margin-right: 5px;">
-                    <p class="info-tit">联系电话：</p>
-                    <input class="info-int" id="tel" type="text" placeholder="">
-                </div>
-            </div>
-            <p class="info-tit">我想去的国家：</p>
-            <select class="info-select" name="country" id="country">
-                <option value="美国">美国</option>
-                <option value="加拿大">加拿大</option>
-                <option value="英国">英国</option>
-                <option value="澳洲">澳洲</option>
-                <option value="香港">香港</option>
-                <option value="其他">其他</option>
-            </select>
-            <p class="info-tit">我完成的阶段：</p>
-            <select class="info-select" name="stage" id="stage">
-                <option value="本科">本科</option>
-                <option value="硕士">硕士</option>
-                <option value="博士">博士</option>
-            </select>
-            <p class="info-tit">我计划出国的时间：</p>
-            <select class="info-select" name="data" id="data">
-                <option value="2018年入学">2018年入学</option>
-                <option value="2017年春季入学">2017年春季入学</option>
-                <option value="2017年秋季入学">2017年秋季入学</option>
-                <option value="2018年春季入学">2018年春季入学</option>
-            </select>
-            <h1 onclick="lxfa()" class="info-btn tm">获取方案</h1>
-        </div>
-
-    </div>
-    <script type="text/javascript">
-        function lxfa() {
-            var name = $("#username").val();
-            var phone = $("#tel").val();
-            var country = $("#country").val();
-            var stage = $("#stage").val();
-            var data = $("#data").val();
-            if (name==''||phone==''){
-                alert("请输入姓名/电话");
-            }else {
-                $.post('/cn/api/smart', {
-                    name: name,
-                    country: country,
-                    phone: phone,
-                    stage: stage,
-                    data: data
-                }, function (re) {
-                    alert(re.message);
-                }, "json")
-            }
-
-        }
-    </script>-->
     <!---------------------------左边悬浮窗---------------------------------->
     <?php use app\commands\front\LeftSuspensionWidget;?>
     <?php LeftSuspensionWidget::begin();?>
@@ -237,7 +175,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="http://sat.thinkuedu.com/" target="_blank">
+                    <a href="http://sat.viplgw.cn/" target="_blank">
                         <img src="/cn/images/intensive_img04.png"/>
                         <p>SAT课程</p>
                     </a>
