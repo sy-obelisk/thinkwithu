dac3d7fc176c162142ecd746431c04d7cea7d0e9
oathYc <cy.y@thinkuedu.com>
2018-09-21 11:27:21 +0800

    gre

--- modules/cn/controllers/GreController.php
+++ modules/cn/controllers/GreController.php
@@ -4,17 +4,17 @@
  * Created by PhpStorm.
  */
 namespace app\modules\cn\controllers;
-use yii;
+use app\libs\Method;
 use app\libs\ThinkUController;
-use app\modules\cn\models\Content;
-use app\modules\cn\models\CategoryExtend;
 
 
 class GreController extends ThinkUController {
     public $enableCsrfValidation = false;
     public $layout = 'cn';
     public function actionIndex(){
-
-        return $this->renderPartial('index');
+        $greContents = Method::post("http://www.greonline.cn/cn/api/gre-index");
+        $greContents = json_decode($greContents,true);
+        $contents = $greContents['data'];
+        return $this->renderPartial('index',$contents);
     }
 }
