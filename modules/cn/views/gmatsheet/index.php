4f63cec78b689352daa3711f298b76c6d451d387
trump <trump.l@thinkwithu.com>
2018-10-18 15:08:41 +0800

    15:08:39

--- modules/cn/views/gmatsheet/index.php
+++ modules/cn/views/gmatsheet/index.php
@@ -4,7 +4,7 @@
 <!--banner-->
 <section class="gmatBanner">
     <div class="banner_cover">
-        <img src="http://www.thinkwithu.com//files/attach/images/20170217/1487326104547250.png" alt="">
+        <img src="/cn/images/gmatImages/gmat-banner.png" alt="">
     </div>
 </section>
 <!--Gmat第一个板块-->
@@ -26,6 +26,7 @@
     </ul>
 </section>
 <!--GMAT出分课程-->
+
 <section class="fraction">
     <div class="fraction_title">
         <h1>99%GMAT 考生出分课程</h1>
@@ -43,87 +44,87 @@
             </ul>
         </div>
         <div class="fraction_details">
-<!--            <div class="content_bd_left">-->
-<!--                <div class="left_name">课程名称</div>-->
-<!--                <div class="left_price">价格</div>-->
-<!--                <div class="left_value">性价比</div>-->
-<!--                <div class="left_startTime">开课时间</div>-->
-<!--                <div class="left_Time">课程时长</div>-->
-<!--                <div class="left_try">试听</div>-->
-<!--                <div class="left_sign">报名</div>-->
-<!--            </div>-->
+            <!--            <div class="content_bd_left">-->
+            <!--                <div class="left_name">课程名称</div>-->
+            <!--                <div class="left_price">价格</div>-->
+            <!--                <div class="left_value">性价比</div>-->
+            <!--                <div class="left_startTime">开课时间</div>-->
+            <!--                <div class="left_Time">课程时长</div>-->
+            <!--                <div class="left_try">试听</div>-->
+            <!--                <div class="left_sign">报名</div>-->
+            <!--            </div>-->
             <div class="content_bd bd">
-<!--                --><?php // if(isset($course['data']['mainCourse'])){
-//                    foreach($course['data']['mainCourse'] as $k=>$v){?>
-                        <ul><!--循环ul-->
-                            <li>
-                                <div class="fraction_name"><!--课程名称-->
-                                    <div class="left_name">课程名称</div>
-                                    <div class="couse">GAMT 晚间强化方法课</div>
-                                    <div class="couse">GAMT 晚间强化方法课</div>
-                                    <div class="couse">GAMT 晚间强化方法课</div>
-                                    <div class="couse">GAMT 晚间强化方法课</div>
-                                    <div class="couse">GAMT 晚间强化方法课</div>
-                                    <div class="couse">GAMT 晚间强化方法课</div>
-                                </div>
-                                <div class="fraction_price"><!--课程价格-->
-                                    <div class="left_price">价格</div>
-                                    <div>¥9000¥9000¥9000¥9000¥9000</div>
-                                    <div>¥9000</div>
-                                    <div>¥9000</div>
-                                    <div>¥9000</div>
-                                    <div>¥9000</div>
-                                    <div>¥9000</div>
-                                </div>
-                                <div class="fraction_value"><!--性价比-->
-                                    <div class="left_value">性价比</div>
-                                    <div>112元/课时112元/课时112元/课时112元/课时</div>
-                                    <div>112元/课时</div>
-                                    <div>112元/课时</div>
-                                    <div>112元/课时</div>
-                                    <div>112元/课时</div>
-                                    <div>112元/课时</div>
-                                </div>
-                                <div class="fraction_startTime"><!--开课时间-->
-                                    <div class="left_startTime">开课时间</div>
-                                    <div>7月31日7月31日7月31日7月31日</div>
-                                    <div>7月31日7月31日7月31日7月31日</div>
-                                    <div>7月31日7月31日7月31日7月31日</div>
-                                    <div>7月31日7月31日7月31日7月31日</div>
-                                    <div>7月31日7月31日7月31日7月31日</div>
-                                    <div>7月31日7月31日7月31日7月31日</div>
-                                </div>
-                                <div class="fraction_Time"><!--课程时长-->
-                                    <div class="left_Time">课程时长</div>
-                                    <div>30+50课程</div>
-                                    <div>30+50课程</div>
-                                    <div>30+50课程</div>
-                                    <div>30+50课程</div>
-                                    <div>30+50课程</div>
-                                    <div>30+50课程</div>
-                                </div>
-                                <div class="fraction_try"><!--试听-->
-                                    <div class="left_try">试听</div>
-                                    <div><a href="" target="_blank">试听</a></div>
-                                    <div><a href="" target="_blank">试听</a></div>
-                                    <div><a href="" target="_blank">试听</a></div>
-                                    <div><a href="" target="_blank">试听</a></div>
-                                    <div><a href="" target="_blank">试听</a></div>
-                                    <div><a href="" target="_blank">试听</a></div>
-                                </div>
-                                <div class="fraction_sign"><!--报名-->
-                                    <div class="left_sign">报名</div>
-                                    <div><a href="" target="_blank">立即报名</a></div>
-                                    <div><a href="" target="_blank">立即报名</a></div>
-                                    <div><a href="" target="_blank">立即报名</a></div>
-                                    <div><a href="" target="_blank">立即报名</a></div>
-                                    <div><a href="" target="_blank">立即报名</a></div>
-                                    <div><a href="" target="_blank">立即报名</a></div>
-                                </div>
-                            </li>
-                        </ul>
-<!--                    --><?php //}
-//                }?>
+                <!--                --><?php // if(isset($course['data']['mainCourse'])){
+                //                    foreach($course['data']['mainCourse'] as $k=>$v){?>
+                <ul><!--外层循环ul  6ge  内部循环li-->
+                    <li>
+                        <div class="fraction_name"><!--课程名称-->
+                            <div class="left_name">课程名称</div>
+                            <div class="couse">GAMT 晚间强化方法课</div>
+                            <div class="couse">GAMT 晚间强化方法课</div>
+                            <div class="couse">GAMT 晚间强化方法课</div>
+                            <div class="couse">GAMT 晚间强化方法课</div>
+                            <div class="couse">GAMT 晚间强化方法课</div>
+                            <div class="couse">GAMT 晚间强化方法课</div>
+                        </div>
+                        <div class="fraction_price"><!--课程价格-->
+                            <div class="left_price">价格</div>
+                            <div>￥9000￥9000￥9000￥9000￥9000</div>
+                            <div>￥9000</div>
+                            <div>￥9000</div>
+                            <div>￥9000</div>
+                            <div>￥9000</div>
+                            <div>￥9000</div>
+                        </div>
+                        <div class="fraction_value"><!--性价比-->
+                            <div class="left_value">性价比</div>
+                            <div>112元/课时112元/课时112元/课时112元/课时</div>
+                            <div>112元/课时</div>
+                            <div>112元/课时</div>
+                            <div>112元/课时</div>
+                            <div>112元/课时</div>
+                            <div>112元/课时</div>
+                        </div>
+                        <div class="fraction_startTime"><!--开课时间-->
+                            <div class="left_startTime">开课时间</div>
+                            <div>7月31日7月31日7月31日7月31日</div>
+                            <div>7月31日7月31日7月31日7月31日</div>
+                            <div>7月31日7月31日7月31日7月31日</div>
+                            <div>7月31日7月31日7月31日7月31日</div>
+                            <div>7月31日7月31日7月31日7月31日</div>
+                            <div>7月31日7月31日7月31日7月31日</div>
+                        </div>
+                        <div class="fraction_Time"><!--课程时长-->
+                            <div class="left_Time">课程时长</div>
+                            <div>30+50课程</div>
+                            <div>30+50课程</div>
+                            <div>30+50课程</div>
+                            <div>30+50课程</div>
+                            <div>30+50课程</div>
+                            <div>30+50课程</div>
+                        </div>
+                        <div class="fraction_try"><!--试听-->
+                            <div class="left_try">试听</div>
+                            <div><a href="" target="_blank">试听</a></div>
+                            <div><a href="" target="_blank">试听</a></div>
+                            <div><a href="" target="_blank">试听</a></div>
+                            <div><a href="" target="_blank">试听</a></div>
+                            <div><a href="" target="_blank">试听</a></div>
+                            <div><a href="" target="_blank">试听</a></div>
+                        </div>
+                        <div class="fraction_sign"><!--报名-->
+                            <div class="left_sign">报名</div>
+                            <div><a href="" target="_blank">立即报名</a></div>
+                            <div><a href="" target="_blank">立即报名</a></div>
+                            <div><a href="" target="_blank">立即报名</a></div>
+                            <div><a href="" target="_blank">立即报名</a></div>
+                            <div><a href="" target="_blank">立即报名</a></div>
+                            <div><a href="" target="_blank">立即报名</a></div>
+                        </div>
+                    </li>
+                </ul>
+                <!--                    --><?php //}
+                //                }?>
                 <ul>
                     第2页
                 </ul>
@@ -275,12 +276,202 @@
                 <?php } ?>
             </div>
         </div>
-        <script type="text/javascript">
-
-        </script>
     </div>
 </section>
+<!--GMAT名师团队-->
+<section class="teacher_index">
+    <div class="w12">
+        <div class="tm gmatsub_tit">
+            <h1>申友网名师与你同行</h1>
+        </div>
+        <div class="picFocus">
+            <div class="bd" id="tabBox1-bd">
+                <ul class="tinter_wrap"><!--循环li-->
+                    <li class="tout_wrap relative">
+                        <div class="teacher_info_wrap">
+                            <p class="t_name">GMAT新派宗师雷哥Kevin</p>
+                            <div class="teacher_tag">
+                                <span>GMAT</span>
+                                <span>留学</span>
+                                <span>职业规划家</span>
+                                <span>英语教育名家</span>
+                            </div>
 
+                            <div class="teacher_cg">
+                                雷哥网联合创始人，留学规划与英语教育名家，GMAT / TOEFL名师。雷哥网联合创始人，中国留学咨询行业领军人物。上海交大EMBA，连续多年亲自深入全球名企名校走访交流，如谷歌、苹果、FACEBOOK，哈佛、沃顿、斯坦福。多年研究英语系国家名校留学，尤其擅长商科申请规划。指导的学生多人获得美英著名商学院录取，如Stanford、MIT、Yale、哥大、JHU、剑桥、LSE、帝国理工等。对文书创作颇有建树，研究名校文书上千篇，洞悉名校录取官喜好和录取倾向。留学考试学术成果：GMAT逻辑靶向图、GMAT逻辑本质论、GMAT语法优先原则、GMAT必要性阅读法、新托福听力阈值训练法、新托福万能口语法、雅思西方批判式作文法等，是留学英语考试的集大成者。在国内新创新托福四化一中心作文法。研发和出版多本留学英语培训教材，并不断优化教学方法，专著有 《Influence--雷哥GMAT逻辑》、 《雅思写作攻略》、 《申友GMAT高分讲义》、 《GMAT真题名师精讲》等。留学规划咨询成果：多年研究英语系国家名校留学，尤其擅长金融、商科和MBA申请规划。所指导的客户多人获得美国、英国著名商学院的录取，如Stanford、MIT、Yale、哥大、JHU、剑桥、LSE、帝国理工、Warwick 、HEC等。对文书创作颇有建树，研究名校文书上千篇，洞悉名校录取官员的喜好和录取倾向。                            </div>
+
+                        </div>
+                        <div class="teacher_index_big_head ani">
+                            <img src="http://www.gmatonline.cn/files/attach/images/content/20150911/14419615925597.jpg" alt="老师照片"/>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="hd">
+                <div class="picMarquee-left2 relative">
+                    <div class="prev pre_btn"><img src="/cn/images/gmatImages/bk4_crow_left.png" alt=""></div>
+                    <div class="next pre_btn"><img src="/cn/images/gmatImages/bk4_crow_right.png" alt="">
+                    </div>
+                    <div class="bd" style="width: 720px;margin: auto;">
+                        <ul class="teacher_index_head_list flex-container"><!--循环li-->
+                            <li>
+                                <div class="teacher_index_head_small"><img src="http://www.gmatonline.cn/files/attach/images/content/20150911/14419615925597.jpg" alt=""></div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!--GMAT考试工具-->
+<section class="bk5">
+    <div class="w12">
+        <ul id="breadcrumb" class="flex-container">
+            <li>
+                <a href="#" target="_blank">考试工具</a>
+            </li>
+            <li>
+                <a href="" target="_blank">考试时间</a>
+            </li>
+            <li>
+                <a href="" target="_blank">考试地点</a>
+            </li>
+            <li>
+                <a href="" target="_blank">考试内容</a>
+            </li>
+            <li>
+                <a href="" target="_blank">考试流程</a>
+            </li>
+            <li>
+                <a href="" target="_blank">考试费用</a>
+            </li>
+        </ul>
+    </div>
+</section>
+<!--GMAT课程活动 热门资讯-->
+<div class="bk-last anne-flex">
+    <div class="commonZx anne-flex txtMarquee-bottom">
+        <div class="left-zx-img">
+            <img src="/cn/images/gmatImages/subN-silde01.jpg" alt="图片">
+        </div>
+        <div class="right-zx-info bd">
+           <ul><!--循环li-->
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+               <li>
+                   <b>•</b>
+                   <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                   <span>2018-10-10</span>
+                   <div class="clearfix"></div>
+               </li>
+
+           </ul>
+        </div>
+    </div>
+    <div class="commonZx anne-flex txtMarquee-bottom_1">
+        <div class="left-zx-img">
+            <img src="/cn/images/gmatImages/subN-slide02.jpg" alt="图片">
+        </div>
+        <div class="right-zx-info bd">
+            <ul><!--循环li-->
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+                <li>
+                    <b>•</b>
+                    <a href="" title="GMAT数学易错考点提升营，5天进阶Q51！">GMAT数学易错考点提升营，5天进阶Q51！</a>
+                    <span>2018-10-10</span>
+                    <div class="clearfix"></div>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+<!--查看更多-->
+<div class="konw_more"><a href="">查看更多</a></div>
 <script>
     //课程板块
     jQuery(".fraction_content").slide({});
@@ -295,5 +486,27 @@
 
         });
     });
+    //名师专栏
+    jQuery(".picFocus").slide({
+        mainCell: ".tinter_wrap",
+        effect: "leftLoop",
+        interTime: 5000,
+//                autoPlay: true,
+        endFun: function (i) { //高度自适应
+            var bd = document.getElementById("tabBox1-bd");
+            bd.style.height = bd.children[0].children[0].children[i + 1].offsetHeight + 50 + "px";
+            if (i > 0)bd.style.transition = "100ms";//添加动画效果
+        }
 
+    });
+    jQuery(".picMarquee-left2").slide({
+        mainCell: ".bd ul",
+        autoPlay: true,
+        effect: "leftMarquee",
+        vis: 7,
+        interTime: 100
+    });
+    //课程活动 热门资讯
+    jQuery(".txtMarquee-bottom").slide({mainCell:".bd ul",autoPlay:true,effect:"topMarquee",vis:8,interTime:50,trigger:"click"});
+    jQuery(".txtMarquee-bottom_1").slide({mainCell:".bd ul",autoPlay:true,effect:"topMarquee",vis:8,interTime:50,trigger:"click"});
 </script>
