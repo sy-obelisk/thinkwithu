141a8a6fd59433075dd6c5f4b849090760c7e19f
trump <trump.l@thinkwithu.com>
2018-11-26 11:57:11 +0800

    11:57:10

--- modules/cn/views/case/details.php
+++ modules/cn/views/case/details.php
@@ -2,11 +2,51 @@
 
 <section class="caseDetails">
     <div class="detailsCon">
-        <div>
-            <a href="">留学商城</a>
-            <a href="">案例库</a>
-            <a href="">最新案例</a>
-            <span>正文</span>
+        <div class="con_title">
+            <a href="">留学商城 ></a>
+            <a href="">案例库 ></a>
+            <a href="">最新案例 ></a>
+            <span> 正文</span>
         </div>
     </div>
+    <div class="content">
+        <!--左边-->
+        <div class="content_left">
+            <p class="left_title">纽约大学！</p><!--标题-->
+            <p class="left_time">来源:2018-11-26 10:56:35</p><!--时间-->
+            <div class="con_c"><!--内容-->
+
+            </div>
+        </div>
+        <!--右边-->
+        <div class="content_right">
+            <div class="right_title">
+                <span class="right_new">最新案例</span>
+                <span class="right_more"><a href="" target="_blank">More>></a></span>
+            </div>
+            <div class="right_c">
+                <ul><!--循环li-->
+                    <li>
+                        <b>•</b>
+                        <a href="">GMAT备考中独立主格结构讲解GMAT备考中独立主格结构讲解</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">GMAT备考中独立主格结构讲解GMAT备考中独立主格结构讲解</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">GMAT备考中独立主格结构讲解GMAT备考中独立主格结构讲解</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">GMAT备考中独立主格结构讲解GMAT备考中独立主格结构讲解</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <script>
+        jQuery(".content_right").slide({mainCell:".right_c ul",autoPlay:true,effect:"topMarquee",vis:10,interTime:50,trigger:"click"});
+    </script>
 </section>
