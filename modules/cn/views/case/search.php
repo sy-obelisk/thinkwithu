6b91c0936ee12578306a07921155842bad0631f8
trump <trump.l@thinkwithu.com>
2018-11-29 17:09:02 +0800

    17:09:00

--- modules/cn/views/case/search.php
+++ modules/cn/views/case/search.php
@@ -5,9 +5,76 @@
     <div class="detailsCon">
         <div class="con_title">
             <a href="">留学商城 ></a>
-            <a href="">案例库 ></a>
-            <a href="">最新案例 ></a>
-            <span> 正文</span>
+            <a href="">案例库</a>
         </div>
     </div>
+    <div class="content">
+        <!--左边-->
+        <div class="content_left">
+            <ul class="search_list">
+                <li>
+                    <div class="result_img">
+                        <img src="http://www.smartapply.cn/files/attach/images/20170328/1490665185318340.png" alt="">
+                    </div>
+                    <div class="result_name">
+                        <p>牛津大学录取&清华大学录取</p>
+                        <a href="">[点击详情]</a>
+                    </div>
+                    <div class="result_time">
+                       <p>2018-11-23 14:00</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="result_img">
+                        <img src="http://www.smartapply.cn/files/attach/images/20170328/1490665185318340.png" alt="">
+                    </div>
+                    <div class="result_name">
+                        <p>牛津大学录取&清华大学录取</p>
+                        <a href="">[点击详情]</a>
+                    </div>
+                    <div class="result_time">
+                        <p>2018-11-23 14:00</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!--右边-->
+        <div class="content_right">
+            <div class="right_title">
+                <span class="right_new">热门案例</span>
+                <span class="right_more"><a href="" target="_blank">More>></a></span>
+            </div>
+            <div class="right_c">
+                <ul><!--循环li-->
+                    <li>
+                        <b>•</b>
+                        <a href="">武汉Guo同学获全美专排第三UIUC建筑专业Offer！</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">武汉Guo同学获全美专排第三UIUC建筑专业Offer！</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">武汉Guo同学获全美专排第三UIUC建筑专业Offer！</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">武汉Guo同学获全美专排第三UIUC建筑专业Offer！</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">武汉Guo同学获全美专排第三UIUC建筑专业Offer！</a>
+                    </li>
+                    <li>
+                        <b>•</b>
+                        <a href="">武汉Guo同学获全美专排第三UIUC建筑专业Offer！</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <script>
+        jQuery(".content_right").slide({mainCell:".right_c ul",autoPlay:true,effect:"topMarquee",vis:10,interTime:50,trigger:"click"});
+    </script>
 </section>
