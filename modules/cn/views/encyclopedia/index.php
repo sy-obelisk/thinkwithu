f09bfa0e559bb4d6fb4456be85b6aa77a38a14db
trump <trump.l@thinkwithu.com>
2018-12-04 16:30:02 +0800

    16:30:00

--- modules/cn/views/encyclopedia/index.php
+++ modules/cn/views/encyclopedia/index.php
@@ -1,7 +1,221 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/12/4
- * Time: 9:23
- */
+<link rel="stylesheet" href="/cn/css/encyclopedia/encyIndex.css">
+<link rel="stylesheet" href="/cn/css/encyclopedia/reset.css">
+
+<section class="encyCover">
+    <div class="w12 bg_w">
+        <!--        banner-->
+        <div class="bannerWrap">
+            <!--左边轮播-->
+            <div class="banner_l">
+                <div class="bannerHd hd">
+                    <ul><!--循环li-->
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="bannerBd bd">
+                    <ul><!--循环li-->
+                        <li>
+                            <a href=""> <img src="http://www.smartapply.cn/files/attach/images/20180228/1519807830738702.jpg" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""> <img src="http://www.smartapply.cn/files/attach/images/20180228/1519807830738702.jpg" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""> <img src="http://www.smartapply.cn/files/attach/images/20180228/1519807830738702.jpg" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!--右边列表-->
+            <div class="banner_r">
+                <div class="br_tit inm">
+                    <span>本科头条</span>
+                    <img src="/cn/images/ency/hot.png" alt="">
+                </div>
+                <ul class="br_news_list">
+                    <li>
+                        <p class="br_name ellipsis"><a href="" target="_blank">2018年TOP30美国大学早录取要求</a></p>
+                        <p class="ellipsis-2 br_de">    在2018年本科托福要求，美国名校说的是100，但是事实上你也看到了，没有110不要开玩笑了…除非你是美高的，本身语言运用能力其实要比国内纯应试英语宝宝的英语能力强得多的那种。</p>
+                        <div class="flex-container">
+                            <span class="br_date">2018-03-01</span>
+                            <a class="br_online" href="#">在线咨询</a>
+                        </div>
+                    </li>
+                    <li>
+                        <p class="br_name ellipsis"><a href="" target="_blank">3月10日哪四所英国名校招生官将来华巡讲？</a></p>
+                        <p class="ellipsis-2 br_de">  随着英国首相梅姨成功访华之后，中英两国的合作关系俨然进入“黄金阶段”，不少家长们为了顺应时代潮流，纷纷希望孩子能尽早的适应国外先进的—英式教育，那如何选择真正适合孩子发展的英国院校?</p>
+                        <div class="flex-container">
+                            <span class="br_date">2018-02-28</span>
+                            <a class="br_online" href="#">在线咨询</a>
+                        </div>
+                    </li>
+                    <li>
+                        <p class="br_name ellipsis"><a href="" target="_blank">美国本科申请课外活动介绍需要注意这六大问题</a></p>
+                        <p class="ellipsis-2 br_de">美国本科申请时，课外活动介绍该如何写？想要体现自己的个人才艺和能力，很多学生都会将自己参加过的课外活动不分主次，罗列于表单上，这样的填写对于自己并不益处。列举课外活动的顺序，不仅仅要列出重要的活动，同时要进行有序的排列，参加的比赛或加入的俱乐部尽量写全称，不要用缩写。跟着小编来看看美国本科申请课外活动介绍需要注意哪些问题?</p>
+                        <div class="flex-container">
+                            <span class="br_date">2018-02-24</span>
+                            <a class="br_online" href="#">在线咨询</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <script>
+            jQuery(".banner_l").slide({mainCell:".bd ul",effect:"leftLoop",autoPlay:true});
+        </script>
+        <!--        留学咨询-->
+        <div>
+            <!--留学资讯-->
+            <div class="flex-container information_nav">
+                <div class="flex-container">
+                    <div class="infor_name">
+                        <img src="/cn/images/ency/zi_x.png" alt="">
+                        <span>留学资讯</span>
+                    </div>
+                    <div class="infor_tel">
+                        <img src="/cn/images/ency/phone.png" alt="">
+                        <span>400-1816-180</span>
+                    </div>
+                </div>
+                <dl class="country_list">
+                    <dt class="on">美国</dt>
+                    <dt class="">英国</dt>
+                    <dt class="">澳洲</dt>
+                    <dt class="">香港</dt>
+                    <dt class="">新加坡</dt>
+                </dl>
+            </div>
+            <div class="item_Wrap" style="padding-bottom: 40px;">
+                <div class="item_tab"><!--外层循环item_tab-->
+                    <div class="flex-container item_tab_wrap"><!--内层循环 item_list-->
+                        <div class="item_list">
+                            <div class="t1_bg item_bg tm">
+                                <div class="item_icon"><img src="/cn/images/ency/item_c1.png" alt=""></div>
+                                <p class="item_name">A 本科院校</p>
+                            </div>
+                            <ul class="news_list">
+                                <li>
+                                    <a href="/information/detail-442-4973.html" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">2018年TOP30美国大学早录取要求</p>
+                                            <p class="news_de ellipsis-2">    在2018年本科托福要求，美国名校说的是100，但是事实上你也看到了，没有110不要开玩笑了…除非你是美高的，本身语言运用能力其实要比国内纯应试英语宝宝的英语能力强得多的那种。</p>
+                                            <div class="news_date">2018-03-01</div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/information/detail-442-4846.html" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">科普贴 | 美国本科提前录取的学校增多！有机会去好学校了</p>
+                                            <p class="news_de ellipsis-2">2017年美国本科申请的一部分已经完结，从这当中多少可以窥见2018年美国留学申请以及政策上的一些变化。</p>
+                                            <div class="news_date">2018-02-05</div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/information/detail-442-4732.html" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">美国本科留学：文理学院有什么优势？</p>
+                                            <p class="news_de ellipsis-2">美国本科留学：文理学院有什么优势？</p>
+                                            <div class="news_date">2018-01-20</div>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="online_infor tm">
+                                <a href="">在线咨询</a>
+                            </div>
+                        </div>
+                        <div class="item_list">
+                            <div class="t2_bg item_bg tm">
+                                <div class="item_icon"><img src="/cn/images/ency/item_c2.png" alt=""></div>
+                                <p class="item_name">B 留学费用</p>
+                            </div>
+                            <ul class="news_list">
+                                <li>
+                                    <a href="" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">NU学费让国际生享有和本土学生一样的待遇</p>
+                                            <p class="news_de ellipsis-2"> 相信想要留学的同学们都知道，出国留学，国外的学校对于国际学生列出的费用是相对要高一些的，那有没有和本土学生同学费的学校呢？答案是肯定的！下面小编就为大家介绍一下这所与众不同，为国际学生带来福音的学校：美国国立大学！</p>
+                                            <div class="news_date">2017-11-09</div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">2018美国本科留学一年费用多少？</p>
+                                            <p class="news_de ellipsis-2">对于留学生而言，美国这样的教育大国是他们心心念念想要去的国家，但是高昂的学费、生活费等等是申请者不得不面对的现实。那么美国留学一年费用多少呢？</p>
+                                            <div class="news_date">2017-11-09</div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">美国本科留学费用最低的五所大学有哪些？</p>
+                                            <p class="news_de ellipsis-2"> 美国本科留学费用最低的五所大学，是否美国名校留学费用都很高呢?美国哪些大学学费比较低,而且是非常不错的大学?下面我们就来盘点一下美国本科留学费用最低的五所大学，并进行详细介绍。</p>
+                                            <div class="news_date">2017-11-09</div>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="online_infor tm">
+                                <a href="">在线咨询</a>
+                            </div>
+                        </div>
+                        <div class="item_list">
+                            <div class="t3_bg item_bg tm">
+                                <div class="item_icon"><img src="/cn/images/ency/item_c3.png" alt=""></div>
+                                <p class="item_name">C 申请条件</p>
+                            </div>
+                            <ul class="news_list">
+                                <li>
+                                    <a href="" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">2018年美本留学5大变化，现在给孩子计划好了可能也并没什么用！</p>
+                                            <p class="news_de ellipsis-2">       再苦不能苦孩子，再穷不能穷教育。这不，许多家长都想着把孩子送出国，接受精英教育。可是计划赶不上变化，准备送孩子去美国的家长们要注意啦！！2018年美国本科留学有以下5大变化，你得搞清楚了！</p>
+                                            <div class="news_date">2018-02-06</div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">想申请到美国本科学校？这几点做好了，拿到Offer是必须的！</p>
+                                            <p class="news_de ellipsis-2">大家都在为美国本科申请而努力，如果你也是准备本科出国的话呢，那么可能会不知道如何申请美国本科，其实想要前往美国留学也不是大家想的那么苦难的，那么下面雷哥留学网就为大家介绍一下2018申请美国本科流程步骤吧!
+                                            </p>
+                                            <div class="news_date">2018-02-05</div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" target="_blank">
+                                        <div>
+                                            <p class="news_tit ellipsis">2018年美国本科留学需要哪些语言考试？</p>
+                                            <p class="news_de ellipsis-2">2018年美国本科留学需要哪些语言考试？</p>
+                                            <div class="news_date">2018-01-26</div>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="online_infor tm">
+                                <a href="">在线咨询</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        $('.country_list dt').click(function () {
+            $(this).addClass('on').siblings("dt").removeClass('on');
+            var num = $(this).index();
+            $('.item_Wrap .item_tab').eq(num).show().siblings('.item_tab').hide();
+
+        })
+    </script>
+</section>
