a573a9049493e6dd34a9f814f0ddcac9d90373a9
trump <trump.l@thinkwithu.com>
2018-12-06 14:39:15 +0800

    14:39:13

--- modules/cn/views/encyclopedia/details.php
+++ modules/cn/views/encyclopedia/details.php
@@ -1,7 +1,129 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/12/4
- * Time: 9:26
- */
+<link rel="stylesheet" href="/cn/css/encyclopedia/details.css">
+<link rel="stylesheet" href="/cn/css/encyclopedia/reset.css">
+
+<section class="enDetailsCover">
+    <div class="enWrap">
+        <div class="enTitle">
+            <a href="">留学商城 ></a>
+            <a href="">头条 ></a>
+            <span>正文</span>
+        </div>
+        <div class="content">
+            <!--左-->
+            <div class="content_left">
+                <!--标题-->
+                <p class="conTitle">哥伦比亚大学申请条件及留学费用【常春藤系列】哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用</p>
+                <div class="consult_view clearfix">
+                    <div class="fl">
+                        <div class="xiaobian_tx">
+                            <a href="">
+                                <img src="http://www.smartapply.cn/cn/images/editor-user/JL.png" alt="小编头像">
+                            </a>
+                        </div>
+                        <span class="xiaob_name">
+                            <a href="javascript:void(0);">申友</a>
+                        </span>
+                        <span>2017-03-03 17:54:53</span>
+                        <span class="view_se">阅读（141）</span>
+                    </div>
+                </div>
+                <!--内容-->
+                <div class="con_con">
+                    哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用哥伦比亚大学申请条件及留学费用
+                </div>
+                <!--尾部-->
+                <div class="con_bot">
+                    <div class="bot_l">
+                        <img src="/cn/images/ency/bq.png" alt="">
+                        <span>硕士留学 美国留学 留学申请</span>
+                    </div>
+                    <div class="bot_r">
+                        <div class="bshare-custom icon-medium sharemore">
+                            <a title="更多平台" class="bshare-more share"><img src="/cn/images/ency/share.png" alt=""><em>分享</em></a>
+                        </div>
+                        <script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh"></script>
+                        <script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script>
+                        <span class="error"><img src="/cn/images/ency/error.png" alt=""><em>报错</em></span>
+                    </div>
+                </div>
+            </div>
+            <!--右-->
+            <div class="study-right">
+                <div class="TopList">
+                    <div class="common-title-h">您可能感兴趣的学校</div>
+                    <div class="interSchool">
+                        <ul><!--循环li-->
+                            <li>
+                                <div class="inter-num">1</div>
+                                <div class="inter-img">
+                                    <img src="http://schools.smartapply.cn/files/2015/0115/20150115033833448.jpg" alt="学校图片">
+                                </div>
+                                <div class="inter-info">
+                                    <h4><a href="/schools/11664.html">波士顿学院                                                    （Boston College）</a></h4>
+                                    <span>排名：<b>32</b> | 查看人数：<b>5056</b></span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </li>
+                            <li>
+                                <div class="inter-num">2</div>
+                                <div class="inter-img">
+                                    <img src="http://schools.smartapply.cn/files/2015/0115/20150115034354272.png" alt="学校图片">
+                                </div>
+                                <div class="inter-info">
+                                    <h4><a href="/schools/11665.html">威廉玛丽学院                                                    （College of William and Mary）</a></h4>
+                                    <span>排名：<b>32</b> | 查看人数：<b>48</b></span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </li>
+                            <li>
+                                <div class="inter-num">3</div>
+                                <div class="inter-img">
+                                    <img src="http://schools.smartapply.cn/files/2015/0115/20150115034548977.png" alt="学校图片">
+                                </div>
+                                <div class="inter-info">
+                                    <h4><a href="/schools/11666.html">纽约大学（NYU）                                                    （New York University）</a></h4>
+                                    <span>排名：<b>30</b> | 查看人数：<b>55</b></span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </li>
+                            <li>
+                                <div class="inter-num">4</div>
+                                <div class="inter-img">
+                                    <img src="http://schools.smartapply.cn/files/2015/0115/20150115034659400.jpg" alt="学校图片">
+                                </div>
+                                <div class="inter-info">
+                                    <h4><a href="/schools/11667.html">罗切斯特大学                                                    （University of Rochester）</a></h4>
+                                    <span>排名：<b>34</b> | 查看人数：<b>36</b></span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </li>
+                            <li>
+                                <div class="inter-num">5</div>
+                                <div class="inter-img">
+                                    <img src="http://schools.smartapply.cn/files/2015/0115/20150115034807979.jpg" alt="学校图片">
+                                </div>
+                                <div class="inter-info">
+                                    <h4><a href="/schools/11668.html">布兰迪斯大学                                                    （Brandeis University）</a></h4>
+                                    <span>排名：<b>34</b> | 查看人数：<b>62</b></span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </li>
+                            <li>
+                                <div class="inter-num">6</div>
+                                <div class="inter-img">
+                                    <img src="http://schools.smartapply.cn/files/2015/0115/20150115034934768.jpg" alt="学校图片">
+                                </div>
+                                <div class="inter-info">
+                                    <h4><a href="/schools/11669.html">佐治亚理工学院                                                    （Georgia Institute of Technology）</a></h4>
+                                    <span>排名：<b>34</b> | 查看人数：<b>73</b></span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
