a8633b83b382176c57a431d5ca89248d70b07e04
trump <trump.l@thinkwithu.com>
2019-01-04 16:24:46 +0800

    16:24:44

--- modules/cn/views/schools/majorDetails.php
+++ modules/cn/views/schools/majorDetails.php
@@ -1,1 +1,178 @@
-专业详情
+<link rel="stylesheet" href="/cn/schools/css/majorDetails.css">
+
+
+<div class="majorDetailsTitle">
+    <a href="">专业解析首页 > </a><span class="titleName">金融硕士</span>
+</div>
+<section class="majorDetails">
+    <!--左边-->
+    <div class="analysisLeft">
+        <div class="leftTitle">
+            <p>金融硕士<span>(Master)</span></p>
+        </div>
+        <div class="leftContent">
+            <!--专业详情-->
+            <div class="proDetails">
+                <p class="proTitle"><b>|</b> 专业详情</p>
+                <div class="proCenter">
+                    <div class="dis_flex">
+                        <div class="wd_1">开始学位</div>
+                        <div class="wd_2">
+                            MasterMasterMaster
+                        </div>
+                    </div>
+                    <div class="dis_flex">
+                        <div class="wd_1">就业方向</div>
+                        <div class="wd_2">
+                            就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向
+                        </div>
+                    </div>
+                    <div class="dis_flex">
+                        <div class="wd_1">从事职业</div>
+                        <div class="wd_2">
+                            就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向就业方向
+                        </div>
+                    </div>
+                    <div class="dis_flex">
+                        <div class="wd_1">相关证书</div>
+                        <div class="wd_2">
+                            MasterMasterMaster
+                        </div>
+                    </div>
+                    <div class="dis_flex">
+                        <div class="wd_1">开始学位</div>
+                        <div class="wd_2">
+                            MasterMasterMaster
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--专业解释-->
+            <div class="explanation">
+                <p class="proTitle"><b>|</b> 专业解释</p>
+                <div class="expContent">
+                    专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释
+                </div>
+            </div>
+            <!--适合申请人背景-->
+            <div class="applicant">
+                <p class="proTitle"><b>|</b> 适合申请人背景</p>
+                <div class="expContent">
+                    专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释专业解释
+                </div>
+            </div>
+            <!--相关院校-->
+            <div class="related">
+                <p class="proTitle"><b>|</b> 相关院校</p>
+                <ul class="relatedName">
+                    <li>麻省理工学院</li>
+                    <li>麻省理工学院</li>
+                    <li>麻省理工学院</li>
+                    <li>麻省理工学院</li>
+                    <li>清华大学</li>
+                    <li>清华大学</li>
+                    <li>清华大学</li>
+                    <li>清华大学</li>
+                </ul>
+            </div>
+            <!--专业开设课程及课程方向-->
+            <div class="courseDirection">
+                <p class="proTitle"><b>|</b> 专业开设课程及课程方向</p>
+                <!--课程方向-->
+                <div class="flex_div">
+                    <div class="wd_3">课程方向</div>
+                    <div class="dotton"></div>
+                </div>
+                <div>
+                    1、公司金融 1、公司金融 1、公司金融 1、公司金融1、公司金融1、公司金融1、公司金融1、公司金融1、公司金融
+                </div>
+                <!--专业开设课程-->
+                <div class="flex_div">
+                    <div class="wd_3">专业开设课程</div>
+                    <div class="dotton"></div>
+                </div>
+                <ul class="relatedName">
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                    <li>公司金融</li>
+                </ul>
+            </div>
+            <!--在线咨询-->
+            <a class="zx_online" href="" target="_blank"><img src="/cn/schools/images/zx_icon.png" alt=""><span>在线咨询</span></a>
+        </div>
+    </div>
+    <!--右边-->
+    <div class="analysisRight">
+        <div class="RightTitle">帮我推荐好顾问</div>
+        <div class="RightSubmit">
+            <p class="m20">你的姓名:</p>
+            <input type="text" class="userName w80" name="userName"/>
+
+            <p class="m10">意向留学国家:</p>
+            <input type="text" class="country w80" name="country"/>
+
+            <p class="m10">个人情况(如就读学校,年级,平均成绩等):</p>
+            <input type="text" class="situation w80" name="situation"/>
+
+            <p class="m10">您的电话<b>*</b>:</p>
+            <input type="text" class="phone w80" name="phone" id="tel"/>
+
+            <input type="text" class="code_marjo" name="code">
+            <button class="getCode" onclick="getCode(this)">获取验证码</button>
+            <div class="sure">提交</div>
+        </div>
+        <div class="rightCenter">
+            <div>
+                <a href="" target="_blank">
+                    <img src="/cn/schools/images/background.png" alt="">
+                    <p>背景测评</p>
+                </a>
+            </div>
+            <div>
+                <a href="" target="_blank">
+                    <img src="/cn/schools/images/choose-school.png" alt="">
+                    <p>选校测评</p>
+                </a>
+            </div>
+            <div>
+                <a href="" target="_blank">
+                    <img src="/cn/schools/images/admission.png" alt="">
+                    <p>录取几率</p>
+                </a>
+            </div>
+        </div>
+        <div class="RightTitle">如何寻找合适的中介公司？</div>
+        <div class="intermediary">
+            <ul>
+                <li>
+                    <img src="/cn/schools/images/public-praise.png" alt="">
+                    <b>看口碑</b>
+                    <span>看学生真实点评</span>
+                </li>
+                <li>
+                    <img src="/cn/schools/images/plan.png" alt="">
+                    <b>比方案</b>
+                    <span>比三套设计方案</span>
+                </li>
+                <li>
+                    <img src="/cn/schools/images/quote.png" alt="">
+                    <b>比报价</b>
+                    <span>比三家公司报价</span>
+                </li>
+                <li>
+                    <img src="/cn/schools/images/guarantee.png" alt="">
+                    <b>比保障</b>
+                    <span>比服务和保障</span>
+                </li>
+            </ul>
+            <div class="lineOn">
+                <a href="" target="_blank">在线咨询</a>
+            </div>
+        </div>
+    </div>
+</section>
