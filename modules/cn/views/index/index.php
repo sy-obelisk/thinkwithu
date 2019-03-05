3674ba21a6fd4b0f587c3c8d3362879370e5c303
trump <trump.l@thinkwithu.com>
2019-03-05 18:11:18 +0800

    18:11:16

--- modules/cn/views/index/index.php
+++ modules/cn/views/index/index.php
@@ -111,7 +111,7 @@
     <link rel="stylesheet" href="/cn/css/footer.css"/>
     <link rel="stylesheet" href="/cn/css/public.css"/>
     <link rel="stylesheet" href="/cn/Hirsi/css/reset.css">
-    <link rel="stylesheet" href="/cn/Hirsi/css/index.css?v=1.2">
+    <link rel="stylesheet" href="/cn/Hirsi/css/index.css?v=1.3">
     <link rel="stylesheet" type="text/css" href="/cn/css/iconfont.css">
     <link rel="stylesheet" href="/cn/css/fonts/font-awesome/css/font-awesome.min.css"/>
     <link rel="shortcut icon" href="http://www.thinkwithu.com/favicon.ico"/>
@@ -251,35 +251,40 @@
                 <li>
                     <img src="/cn/Hirsi/images/test-1.png" alt="">
                     <div class="eaxm_bot">
-                        <p>GMAT</p>
+                        <p>申友GMAT</p>
+                        <p>提分课程</p>
                         <a href="/gmatsheet.html">查看详情</a>
                     </div>
                 </li>
                 <li>
                     <img src="/cn/Hirsi/images/test-2.png" alt="">
                     <div class="eaxm_bot">
-                        <p>GRE</p>
+                        <p>申友GRE</p>
+                        <p>提分课程</p>
                         <a href="/gre.html">查看详情</a>
                     </div>
                 </li>
                 <li>
                     <img src="/cn/Hirsi/images/test-3.png" alt="">
                     <div class="eaxm_bot">
-                        <p>托福</p>
+                        <p>申友托福</p>
+                        <p>提分课程</p>
                         <a href="/toefl/assistance.html">查看详情</a>
                     </div>
                 </li>
                 <li>
                     <img src="/cn/Hirsi/images/test-4.png" alt="">
                     <div class="eaxm_bot">
-                        <p>雅思</p>
+                        <p>申友雅思</p>
+                        <p>提分课程</p>
                         <a href="/ieltssheet.html">查看详情</a>
                     </div>
                 </li>
                 <li>
                     <img src="/cn/Hirsi/images/test-5.png" alt="">
                     <div class="eaxm_bot">
-                        <p>SAT</p>
+                        <p>申友SAT</p>
+                        <p>提分课程</p>
                         <a href="/satsheet.html">查看详情</a>
                     </div>
                 </li>
@@ -1180,9 +1185,10 @@ else{
         titCell:".hd ul li",
         mainCell: ".bdCurriculum ul",
         effect: "left",
-        trigger: "click",
+        trigger: "mouseover",
         delayTime: 1000,
         interTime: 3000,
+        autoPlay:true,
         mouseOverStop: true
     });
     //名师团队
